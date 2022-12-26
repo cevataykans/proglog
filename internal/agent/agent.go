d17cc43e22ad8d9148f59640d00480245365d9e3
cevataykans <aykansevinc66@gmail.com>
2022-12-26 17:44:12 +0100

    Add agent that handles discovery, replication, setup

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -6,9 +6,15 @@ import (
 	"net"
 	"sync"
 
+	api "github.com/cevataykans/proglog/api/v1"
+	"github.com/cevataykans/proglog/internal/auth"
 	"github.com/cevataykans/proglog/internal/discovery"
 	"github.com/cevataykans/proglog/internal/log"
+	"github.com/cevataykans/proglog/internal/server"
+
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type Agent struct {
@@ -65,19 +71,110 @@ func (c Config) RPCAddr() (string, error) {
 }
 
 func (a *Agent) setupLogger() error {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		return err
+	}
+	zap.ReplaceGlobals(logger)
+	return nil
+}
+
+func (a *Agent) setupLog() error {
 	var err error
 	a.log, err = log.NewLog(a.Config.DataDir, log.Config{})
 	return err
 }
 
-func (a *Agent) setupLog() error {
-
-}
-
 func (a *Agent) setupServer() error {
+	authorizer := auth.New(a.ACLModelFile, a.ACLPolicyFile)
 
+	serverConfig := &server.Config{
+		CommitLog:  a.log,
+		Authorizer: authorizer,
+	}
+
+	var opts []grpc.ServerOption
+	if a.Config.ServerTLSConfig != nil {
+		creds := credentials.NewTLS(a.Config.ServerTLSConfig)
+		opts = append(opts, grpc.Creds(creds))
+	}
+	var err error
+	a.server, err = server.NewGRPCServer(serverConfig, opts...)
+	if err != nil {
+		return err
+	}
+	rpcAddr, err := a.RPCAddr()
+	if err != nil {
+		return err
+	}
+	ln, err := net.Listen("tcp", rpcAddr)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		if err := a.server.Serve(ln); err != nil {
+			_ = a.Shutdown()
+		}
+	}()
+	return err
 }
 
 func (a *Agent) setupMembership() error {
+	rpcAddr, err := a.RPCAddr()
+	if err != nil {
+		return err
+	}
 
+	var opts []grpc.DialOption
+	if a.Config.PeerTLSConfig != nil {
+		opts = append(opts, grpc.WithTransportCredentials(
+			credentials.NewTLS(a.PeerTLSConfig),
+		))
+	}
+	conn, err := grpc.Dial(rpcAddr, opts...)
+	if err != nil {
+		return err
+	}
+	client := api.NewLogClient(conn)
+	a.replicator = &log.Replicator{
+		DialOptions: opts,
+		LocalServer: client,
+	}
+	a.memebrship, err = discovery.New(a.replicator, discovery.Config{
+		NodeName: a.Config.NodeName,
+		BindAddr: a.Config.BindAddr,
+		Tags: map[string]string{
+			"rpc_addr": rpcAddr,
+		},
+		StartJoinAddrs: a.Config.StartJoinAddrs,
+	})
+	return err
+}
+
+func (a *Agent) Shutdown() error {
+	a.shutdownLock.Lock()
+	defer a.shutdownLock.Unlock()
+
+	if a.shutdown {
+		return nil
+	}
+	a.shutdown = true
+	close(a.shutdowns)
+
+	shutdown := []func() error{
+		a.memebrship.Leave,
+		a.replicator.Close,
+		func() error {
+			a.server.GracefulStop()
+			return nil
+		},
+		a.log.Close,
+	}
+	for _, fn := range shutdown {
+		if err := fn(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
